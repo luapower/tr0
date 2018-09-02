68d08a129cde6e247d123f81f13bf18a10044d35
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2018-09-03 02:00:51 +0300

    unimportant

--- tr.lua
+++ tr.lua
@@ -1223,9 +1223,9 @@ end
 function segments:bounding_box()
 	local lines = assert(self.lines, 'text not laid out')
 	local bx = lines.x + lines.min_x
-	local a1 = lines[1].spacing_ascent
-	local by = lines.y + lines.baseline - lines[1].spacing_ascent
 	local bw = lines.max_ax
+	local a1 = lines[1].spacing_ascent
+	local by = lines.y + lines.baseline - a1
 	local bh = a1 + lines[#lines].y - lines[#lines].spacing_descent
 	return bx, by, bw, bh
 end
